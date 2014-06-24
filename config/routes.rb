ca4956887f60e98f64402da079bb34659dbbb8ad
catfriend <eakari@live.com>
2014-06-24 14:45:57 -0700

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Portfolio::Application.routes.draw do
 
   resources :posts do
     resources :comments
-  end
+
 
   resources :projects do
     resources :comments
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
